b58f3d360d4e7a6942acf774e4efc97827f89f92
Robin Lugo Boero <rjlb991102@hotmail.com>
2025-10-13 23:04:36 -0500

    .

--- src/main/java/ec/com/denunciasecuador/feature/denuncia/dto/DenunciaRequestDTO.java
+++ src/main/java/ec/com/denunciasecuador/feature/denuncia/dto/DenunciaRequestDTO.java
@@ -8,6 +8,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,32 +21,33 @@ import lombok.ToString;
 public class DenunciaRequestDTO {
 	
 	@Nonnull
-	@NotBlank(message = "{}")
+	@NotBlank(message = "{TITLE.NOTBLANK}")
+	@Size(min = 5, message = "{TITLE.SIZE}")
 	private String title;
 	
 	@Nonnull
-	@NotNull(message = "{}")
+	@NotNull(message = "{DESCRIPTION.NOTNULL}")
 	private String description;
 	
 	@Nonnull
-	@NotNull(message = "{}")
+	@NotNull(message = "{EVENTTIMESTAMP.NOTNULL}")
 	private LocalDateTime eventTimestamp;
 	
 	@Nonnull
-	@NotBlank(message = "{}")
+	@NotBlank(message = "{CITYPROVINCE.NOTBLANK}")
 	private String cityProvince;
 	
 	@Nonnull
-	@NotNull(message = "{}")
+	@NotNull(message = "{ISPRIVATE.NOTNUL}")
 	private boolean isPrivate;
 	
 	@Nonnull
-	@NotNull(message = "{}")
+	@NotNull(message = "{REPORTTYPE.NOTNULL}")
 	@Enumerated(EnumType.STRING)
 	private TipoDenuncia reportType;
 	
 	@Nonnull
-	@NotNull(message = "{}")
+	@NotNull(message = "{USERID.NOTNULL}")
 	private Long usuario_id;
 	
 }
